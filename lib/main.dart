208431693c8714c8190297eacb7cdc496bb504c3
bruno <bruno.pfsilva@hotmail.com>
2019-07-26 01:40:53 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const url = "api.hgbrasil.com/finance?formt=json&key=216690bb";
+
+void main() => runApp(Home());
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
