18b1408c0b92bb7bebf31c3c43fe518e3d1699e3
bruno <bruno.pfsilva@hotmail.com>
2019-07-27 23:43:06 +0100

    App concluido

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ import 'dart:convert';
 //constante
 const request = "http://api.hgbrasil.com/finance?formt=json&key=216690bb";
 
-double euro;
-double dolar;
 
 Future<Map> getData() async {
   http.Response response = await http.get(request);
@@ -28,6 +26,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+
+  double euro;
+  double dolar;
+  //controllers edit text
+  final realeditControler = TextEditingController();
+  final euroeditControler = TextEditingController();
+  final dolareditControler = TextEditingController();
+
+  //text change edittext
+
+  void _realchanged (String text){
+  double real = double.parse(text);
+  dolareditControler.text = (real/dolar).toStringAsPrecision(4);
+  euroeditControler.text = (real/euro).toStringAsPrecision(4);
+  }
+
+  void _eurochanged (String text){
+    double euro = double.parse(text);
+    realeditControler.text = (euro * this.euro).toStringAsPrecision(4);
+    dolareditControler.text = (euro * this.euro / dolar).toStringAsPrecision(4);
+
+
+
+
+  }
+
+  void _dolarchanged (String text){
+    double dolar = double.parse(text);
+    // this.euro; pega a variavel acima nao a de dentro da funcao
+    realeditControler.text = (dolar * this.dolar).toStringAsPrecision(4);
+    euroeditControler.text = (dolar * this.dolar / euro).toStringAsPrecision(4);
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -67,8 +101,7 @@ class _HomeState extends State<Home> {
                       ),
                     );
                   } else {
-                    dolar =
-                        snapshot.data["results"]["currencies"]["USD"]["buy"];
+                    dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
                     euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
 
                     return SingleChildScrollView(
@@ -81,24 +114,12 @@ class _HomeState extends State<Home> {
                             size: 99,
                             color: Colors.lightBlue,
                           ),
-                          TextField(
-                            decoration: InputDecoration(labelText: "Reais",border: OutlineInputBorder(),prefixText: "R\$"),
-                            style: TextStyle(
-                                color: Colors.lightBlue, fontSize: 20),textAlign: TextAlign.center,
-                          ),
+                          buildTextField("Real", "R\$",realeditControler,_realchanged),
                           Divider(),
-                          TextField(
-                            decoration: InputDecoration(labelText: "Euros",border: OutlineInputBorder(),prefixText: "€"),
-                            style: TextStyle(
-                                color: Colors.lightBlue, fontSize: 20),textAlign: TextAlign.center,
-                          ),
+                          buildTextField("Euro", "€",euroeditControler,_eurochanged),
                           //espaço entre os componentes
                           Divider(),
-                          TextField(
-                            decoration: InputDecoration(labelText: "Resultado",border: OutlineInputBorder(),prefixText: "="),
-                            style: TextStyle(
-                                color: Colors.lightBlue, fontSize: 20),textAlign: TextAlign.center,
-                          ),
+                          buildTextField("Dolar", "\$",dolareditControler,_dolarchanged),
                         ],
                       ),
                     );
@@ -107,6 +128,20 @@ class _HomeState extends State<Home> {
               }
             }),
       ),
+    );
+  }
+
+  Widget buildTextField(String label, String prefix, TextEditingController ed ,Function functionTextChange) {
+    return TextField(
+      controller: ed,
+      decoration: InputDecoration(
+          labelText: label, border: OutlineInputBorder(), prefixText: prefix),
+      style: TextStyle(color: Colors.lightBlue, fontSize: 20),
+      //chama a função sempre que o texto muda
+      onChanged: functionTextChange,
+      textAlign: TextAlign.center,
+      //so aceita numeros
+      keyboardType: TextInputType.number,
     );
   }
 }
